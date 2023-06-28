ee6261a0bc67744c8e97bbfcc9026e9b45532516
alex <format37@gmail.com>
2023-06-28 15:53:10 +0400

    modified:   out/README.md 	modified:   run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,16 @@
+# Retrieve the container ID of the container named vosk_cpu_vosk-cpu_1
+container_id=$(sudo docker ps -a | grep vosk_cpu_vosk-cpu_1 | awk '{print $1}')
+
+# Check if the container is running
+if [ -z "$(sudo docker ps -q -f id=$container_id)" ]; then
+    echo "Container is not running. Starting container with id: $container_id"
+    # Start the docker container named vosk_cpu_vosk-cpu_1 if it has not been started yet
+    sudo docker start $container_id
+else
+    echo "Container with id: $container_id is already running. No action is taken."
+fi
+
+
 sudo docker run -it --rm \
   --network=host \
   -v $(pwd)/scripts:/app/scripts \
